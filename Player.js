1aca500759a91c9053ef6ac09dccb5a2e828120c
David Álvarez <davidalvper@gmail.com>
2023-10-20 19:02:35 +0100

    Instalación del jest

---
+++ Player.js
@@ -0,0 +1,14 @@
+class Player {
+    #name;
+
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+
+
+//PRUEBAS (ELIMINAR A PARTIR DE AQUI)
+
+let player1 = new Player('David')
+console.log(player1)

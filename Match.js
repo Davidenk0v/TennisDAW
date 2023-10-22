e053d6f341a9c8b801712f2069db9729f655923d
David <davidalvper@gmail.com>
2023-10-22 19:46:26 +0100

    Terminada la clase Player

--- Match.js
+++ Match.js
@@ -1,4 +1,6 @@
 class Match {
+    #matchDate;
+    #game;
 
     constructor() {
 
--- Player.js
+++ Player.js
@@ -1,12 +1,62 @@
 class Player {
     #name;
-    #ranking
+    #sex;
+    #birthDate;
+    #age;
 
-    constructor(name, ranking) {
+    constructor(name, sex, birthDate) {
         this.name = name;
-        this.ranking = ranking;
+        this.birthDate = birthDate;
+        this.sex = this.validateSex(sex);
+        this.age = this.ageCalculator()
+    }
+
+    validateSex(sex) {
+        if (sex.toLowerCase() === 'male') {
+            return 'Male';
+        } else {
+            return 'Female';
+        }
+    }
+
+    getSex() {
+        return this.#sex;
+    }
+
+    ageCalculator() {
+        let now = new Date();
+        let birth = new Date(this.birthDate);
+        let age = now.getFullYear() - birth.getFullYear();
+        let difmeses = now.getMonth() - birth.getMonth();
+
+        if (difmeses < 0 || (difmeses === 0 && now.getDate() < birth.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    getCategory() {
+        if (this.age < 12) {
+            return 'Benjamin';
+        } else if (this.age > 12 && this.age < 16) {
+            return 'Cadet'
+        } else if (this.age > 15 && this.age < 20) {
+            return 'Junior'
+        } else {
+            return 'Senior'
+        }
     }
 }
+
+
+let player1 = new Player('David', 'hamburguesa', '01-05-1990')
+
+console.log(player1)
+console.log(player1.category())
 
 
 module.exports = Player;
